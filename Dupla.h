0c0d1a1096cfef9ccada170b685448bac4a3ae1d
Antônio Filho <51380277+antonioclf@users.noreply.github.com>
2019-11-20 14:13:59 -0300

    Update Dupla.h

--- Dupla.h
+++ Dupla.h
@@ -9,14 +9,34 @@
 #include<string>
 
 class Dupla{
+    private:
+        int _pontos;
+
     public: 
-        Dupla();
         void pedir_truco();
         void pedir_seis();
         void pedir_nove();
         void pedir_doze();
         void vitoria();
         void derrota();
+    
+        Dupla(int _pontos): pontos(_pontos){}
+    //add seriam os pontos adicionais ( 2 em vitória normal, 4 em truco,etc)
+        void setar_pontos(int pontos, int add){
+            this->pontos = pontos + add;
+        }
+        void vitoria(int add){
+     //if truco aceito e etc
+            add = 4;
+            
+            setar_pontos();
+        }
+        void derrota(int add){
+        // if truco aceito    
+            add = -4
+                
+            setar_pontos();
+        }
 };
 
 #endif
