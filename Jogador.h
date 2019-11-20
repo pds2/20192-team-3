29c20f2f57f77c275f33cc7ade38d40608b8a541
Antônio Filho <51380277+antonioclf@users.noreply.github.com>
2019-11-19 23:05:51 -0300

    Update Jogador.h

--- Jogador.h
+++ Jogador.h
@@ -5,23 +5,22 @@
 #include<iomanip>
 #include<cstdlib>
 #include<ctime>
-#include<"carta.h>
-#include<"baralho.h>
-#include<"dupla.h>
+#include<"carta.h">
+#include<"baralho.h">
+#include<"dupla.h">
 #include<string>
 
 class Jogador{
     public: 
         Jogador();
-        votar_truco();
+        void votar_truco();
         void votar_seis();
         void votar_nove();
-        votar_doze();
-        jogar_carta();
+        void votar_doze();
+        void jogar_carta();
         void cortar_baralho();
         void queimar_mao();
-        void mao; 
-        
+        void mao;     
 };
 
 #endif
