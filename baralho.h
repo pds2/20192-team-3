d9c3a8c8b748a3b51183231ccd73f8bab9c06078
Antônio Filho <51380277+antonioclf@users.noreply.github.com>
2019-11-23 01:34:36 -0300

    Update baralho.h

--- baralho.h
+++ baralho.h
@@ -8,12 +8,12 @@ class Baralho {
     private:
         std::vector<Carta*> baralho;
     public:
-        Baralho(); //Constructor
-        ~Baralho(); //Destructor
-        void embaralhar(); //Shuffle the cards of deck
-        void mostrar_baralho(); //Print all the cards in deck
-        Carta* pegar_carta(); //Draw one card from the top of the deck
-        void inserir_carta(Carta* carta); //Put one card in the deck
+        Baralho(); //Construtor
+        ~Baralho(); //Destrutor
+        void embaralhar(); 
+        void mostrar_baralho(); //cartas do baralho todo
+        Carta* pegar_carta(); //pega uma carta do topo 
+        void inserir_carta(Carta* carta); //por carta no baralho 
 };
 
 #endif
