4e872d56dce9b26312366ee9883671c71a18df81
Antônio Filho <51380277+antonioclf@users.noreply.github.com>
2019-11-23 00:18:58 -0300

    Update jogador.cpp

--- jogador.cpp
+++ jogador.cpp
@@ -1,6 +1,6 @@
 #include "jogador.h"
 
-Player::Player(string nome, int num, bool distr){
+Jogador::Jogador(string nome, int num, bool distr){
   nomeJog = nome;
   jogador = num;
   distribuir = distr;
