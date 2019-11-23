638972d3fc92e48f1e3be2dd6fd3e8b7008cd42c
Antônio Filho <51380277+antonioclf@users.noreply.github.com>
2019-11-23 02:05:43 -0300

    Create mesa.cpp

---
+++ mesa.cpp
@@ -0,0 +1,21 @@
+#include "mesa.h"
+
+Mesa::Mesa(){}
+
+Mesa::~Mesa(){
+    this->mesa.clear();
+}
+
+//devolve as cartas para o baralho
+void Mesa::recolocar_cartas(Baralho* baralho){
+    std::vector<Carta*>::iterator it;
+    for(it = this->mesa.begin(); it != this->mesa.end(); ++it){
+        baralho->inserir_carta(*it);
+    }
+    // this->baralho.clear();
+}
+
+//coloca uma carta na mesa
+void Mesa::por_na_mesa(Carta* carta){
+    this->mesa.push_back(carta);
+}
