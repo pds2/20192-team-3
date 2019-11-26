9a8f8fdac3dbdd59439d38b964a4039f1da4d6bb
Felipe Brina <fbbrina10@gmail.com>
2019-11-25 21:41:14 -0300

    correcao

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 #include "usual_card.h"
 #include "baralho.h"
 #include "mao.h"
-#include <ctime>     
-#include <cstdlib>  // For srand() and rand()   
+#include <ctime>
+#include <cstdlib>  // For srand() and rand()
 
 int main(){
 
     //variables declaration
     bool game_loop = true;
-   
+
     // Card* selectedCard;
 
     //------------------------------------------------------------------------------
-  
+
     //INICIALIZAÇÃO DO JOGO
     Game game = Game();
     Logica logica = Logica();
@@ -42,14 +42,14 @@ int main(){
         logica.controleRodada(&baralho, maoJogador, ia1Mao, ia2Mao, ia3Mao);
 
         game.printMaoVencedora(logica);
-        
+
         std::cout << std::endl;
 
         game.printQuedaVencedor();
-        
+
         //testa condicao de fim da partida (acabou a melhor de três quedas)
         //ATENCAO: essa deve ser a ultima linha dentro do game looping....
-        game_looping = game.check_end();
+        game_loop = game.check_end();
     }
 
     game.print_vencedor();
