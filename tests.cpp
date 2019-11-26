3b84f8aedc8e12e217ff2807738413f04cdbafb0
Felipe Brina <fbbrina10@gmail.com>
2019-11-26 16:50:03 -0300

    Upload do tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -35,21 +35,43 @@ TEST_CASE ("02 - Teste getters e setters do baralho"){
     CHECK(baralho1->get_QuatroDePaus()==QuatroDePaus);
     CHECK(baralho2->get_SeteDeCopas()==SeteDeCopas);
     CHECK(baralho3->get_AsDeEspadas()==AsDeEspadas);
-    CHECK(baralho4->get_SeteDeOuross()==SeteDeOuros);
+    CHECK(baralho4->get_SeteDeOuros()==SeteDeOuros);
 
-    baralho3->set_
-    baralho3->set_
-    baralho3->set_
+    baralho4.set_SeteDeOuros;
+    baralho3.set_AsDeEspadas;
+    baralho2.set_SeteDeCopas;
+    baralho1.set_QuatroDePaus;
 
-    baralho2.set_
-    baralhoo2.set_
-    baralho2.set_
+    CHECK(baralho4->get()=="SeteDeOuros");
+    CHECK(baralho3->get()=="AsDeEspadas");
+    CHECK(baralho2->get()=="SeteDeCopas");
+    CHECK(baralho1->get()=="QuatroDePaus");
+}
 
-    CHECK()
-    CHECK()
-    CHECK()
 
-    CHECK()
-    CHECK()
-    CHECK()
+
+TEST_CASE ("01 - Teste construtor carta"){
+    CHECK_NOTHROW(Carta carta1(""));
+    CHECK_NOTHROW(Carta carta2(""));
+}
+
+TEST_CASE ("02 - Teste getters e setters da carta"){
+    Carta *carta1 = new Carta("");
+    Carta carta2();
+    Carta *carta3 = new Carta("");
+
+    CHECK(carta1->get_Paus()== Paus);
+    CHECK(carta2->get_Copas()== Copas);
+    CHECK(carta3->get_Espadas()== Espadas);
+    CHECK(carta4->get_Ouros()== Ouros);
+
+    carta4.set_Ouros;
+    carta3.set_Espadas;
+    carta2.set_Copas;
+    carta1.set_Paus;
+
+    CHECK(carta4->get()=="Ouros");
+    CHECK(carta3->get()=="Espadas");
+    CHECK(carta2->get()=="Copas");
+    CHECK(carta1->get()=="Paus");
 }
